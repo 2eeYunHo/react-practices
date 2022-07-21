7ef11c126d99d0827fc77d50bbccc95de88908ec
llyn <mmm01230@gmail.com>
2022-07-21 11:21:34 +0900

    제어 컴포넌트 #2

--- 2.component/ex04/src/02/Form.js
+++ 2.component/ex04/src/02/Form.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import './assets/Form.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCheckCircle, faTimesCircle} from '@fortawesome/free-solid-svg-icons';
 
 export default function Form() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [validEmail, setValidEmail] = useState(false);
+    const [password, setPassword] = useState("");
+    const [gender, setGender] = useState("male");
 
     const onChangeNameInput = e => {
         // setName(e.target.value);
@@ -21,6 +25,10 @@ export default function Form() {
         
     }
 
+    const onChangeInputGender = e => {
+        setGender(e.target.value);
+    }
+
     return (
         <form id="joinForm" name="joinForm" method="post" action="/do/not/post">
             <label htmlFor="name">이름</label>
@@ -35,15 +43,38 @@ export default function Form() {
                    name="email" 
                    ype="text" 
                    value={ email }
-                   onChange = {onChangeEmailInput}/>{ validEmail ? "ok" : "no" }
+                   onChange = {onChangeEmailInput}/>
+                   { email === '' ? 
+                        null : 
+                        validEmail ?
+                            <FontAwesomeIcon icon ={faCheckCircle} style={{fontSize:16, color:'blue'}} />:
+                            <FontAwesomeIcon icon ={faTimesCircle} style={{fontSize:16, color:'red'}} />
+                    }
 
             <label htmlFor="password">패스워드</label>
-            <input id="password" name="password" type="password" value={ "" } />
+            <input id="password" 
+                   name="password" 
+                   type="password" 
+                   value={ password } 
+                   onChange = { e => setPassword(e.target.value)}/>
+                   
 
             <fieldset>
                 <legend>성별</legend>
-                <label>여</label> <input type="radio" name="gender" value={ "female" } defaultChecked={ true } />
-                <label>남</label> <input type="radio" name="gender" value={ "male" } defaultChecked={ false } />
+                <label>여</label> 
+                <input type="radio" 
+                       name="gender" 
+                       value={ "female" } 
+                       checked={ gender === 'female' } 
+                       onChange={onChangeInputGender}/>
+
+
+                <label>남</label> 
+                <input type="radio" 
+                       name="gender" 
+                       value={ "male" } 
+                       checked={ gender === 'male' } 
+                       onChange={onChangeInputGender}/>
             </fieldset>
 
             <label htmlFor="birthYear">생년</label>
